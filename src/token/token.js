ecf5c401d81a98b4f8d9ff63c63e9fb5a769f57d
Seongmin Park <seongminnpark@gmail.com>
2018-06-04 01:26:37 +0900

    Fix token

--- src/token/token.js
+++ src/token/token.js
@@ -1,227 +1,186 @@
-// transaction.js
+// token.js
 
 'use strict';
 
-const ethTx = require('ethereumjs-tx');
+const QUINTILLION = 10.0 ** 18; // Ratio of wei to eth.
 
-var okdk;
+var okdk
 
-export default class Transaction {
+import Contract from '../core/contract';
+
+export default class Token extends Contract {
 
   /**
-   * Set up Transaction module.
+   * Set up Token module.
    * @constructor
    *
    * @param {Object} _okdk - OKDk module instance.
    */
   constructor(_okdk) {
-    okdk = _okdk;
+      super(_okdk);
+      okdk = _okdk;
   }
 
   /**
-   * Deploy contract.
-   *
-   * @param {Object} _user - User instance of caller.
-   * @param {string} _password - Password for keystore.
-   * @param {Object} _abi - Abi encoded function call.
-   * @param {Object} _bytecode - Bytecode of abi.
-   * @param {Object} _constructorParams - Constructor parameters in an array.
-   * @param {int} _value - Wei to send to deployed contract.
-   * @param {Object} _options - Optional object containing values of 
-   *                            nonce, gasPrice, and gasLimit to be used.
+   * Initialize Token module.
    */
-  async deployContract(
-    _user, _password, _abi, _bytecode, 
-    _constructorParams, _value, _options = {}) {
-    
-    try {
-      
-      var contract = new okdk.web3.eth.Contract(_abi);
-
-      var deployInfo = contract.deploy({
-        data: _bytecode,
-        arguments: _constructorParams
-      });
-
-      const deployData = deployInfo.encodeABI();
-
-      const result = await this.sendTransaction(
-        _user, null, _value, deployData, _password, _options);
-
-      return result;
-
-    } catch (error) {
-      throw error;
-    }
+  async init() {
+    /* Load abi & address */
+    this._abi = okdk.core._tokenAbi;
+    this._bytecode = okdk.core._tokenBytecode;
+    this.address = okdk.core._tokenAddress;
   }
 
   /**
-   * Call contract requiring transaction with supplied abi.
-   * @ignore
+   * Deploy token to network.
    *
-   * @param {Object} _user - User instance of caller.
+   * @param {Object} _user - User instance of token holder.
    * @param {string} _password - Password to keyfile of user.
-   * @param {string} _address - Address of contract being called.
-   * @param {Object} _callData - Abi encoded function call.
+   * @param {int} _initialSupply - Initial supply of token.
+   * @param {string} _tokenName - Name of token.
+   * @param {string} _tokenSymbol - Token symbol.
    * @param {Object} _options - Optional object containing values of 
    *                            nonce, gasPrice, and gasLimit to be used.
    */
-  async callContract(_user, _password, _address, _callData, _options = {}) {
+  async deploy(_user, _password, 
+      _initialSupply, _tokenName, _tokenSymbol, _options = {}) {
+
     try {
-     
-      const result = await this.sendTransaction(
-        _user, _address, 0, _callData, _password, _options);
+
+      // const initialSupply = okdk.web3.utils.toBN(_initialSupply)
+      //                     .mul(okdk.web3.utils.toBN(QUINTILLION));
+
+      const initialSupply = okdk.web3.utils.toBN(_initialSupply);
+
+      const params = [initialSupply, _tokenName, _tokenSymbol];
+
+      /* Deploy contract with relevant values. */
+      const result = await super.deploy(
+        _user, _password, params, 0, _options);
+
       return result;
+
     } catch (error) {
       throw error;
     }
+  }
+
+  /**
+   * Query OKDK token balance of a user.
+   *
+   * @param {Object} _user - User instance of account to query.
+   */
+  async balanceOf(_user) {
+
+      const address = _user.address;
+      const balance = await this._contract.methods.balanceOf(address).call();
+
+      return this.unitsToTokens(balance);
   };
 
   /**
-   * Send transaction to network.
-   * @ignore
+   * Query OKDK token balance of address string.
    *
-   * @param {Object} _from - User instance of caller.
-   * @param {string} _to - Address receiving transaction. null if contract deployment.
-   * @param {int} _value - Value of transaction.
-   * @param {Object} _data - Data(function call/contract info) to include in transaction.
-   * @param {string} _password - Password to keystore file of caller.
+   * @param {string} _address - Address of account to query.
+   */
+  async balanceOfAddress(_address) {
+
+      const balance = await this._contract.methods.balanceOf(_address).call();
+
+      return this.unitsToTokens(balance);
+  };
+
+  /**
+   * Query OKDK token allowance of a user.
+   *
+   * @param {Object} _user - User instance of account to query.
+   * @param {string} _address - Address of spender.
+   */
+  async getAllowance(_user, _address) {
+
+      const userAddress = _user.address;
+      const balance = await this._contract.methods.allowance(userAddress, _address).call();
+
+      return this.unitsToTokens(balance);
+  };
+
+  /**
+   * Send OKDK token to address.
+   *
+   * @param {Object} _user - User instance of sender.
+   * @param {string} _password - Password to unlock user's keystore.
+   * @param {string} _address - Address to send tokens to.
+   * @param {int} _value - Amount to send.
    * @param {Object} _options - Optional object containing values of 
    *                            nonce, gasPrice, and gasLimit to be used.
    */
-  async sendTransaction(_from, _to, _value, _data, _password, _options = {}) {
+  async transfer(_user, _password, _address, _value, _options = {}) {
     try {
       
-      /* Fetch private key from keystore. */
-      const privateKey = await _from.getPrivateKey(_password);
+      const valueInContract = this.tokensToUnits(_value);
 
-      /* Return error if private key is undefined. */
-      if (privateKey === 'undefined') {
-        throw 'Private key is undefined.';
-      }
+      // Construct abi.
+      let callData =
+        this._contract.methods.transfer(_address, valueInContract).encodeABI();
+        
+      const result =
+        await okdk.core.callContract(_user, _password, this._address, callData, _options);
 
-      var nonce = 0;
-      var gasPrice = 0;
-      var gasLimit = 0;
+      return result;
 
-      /* Generate promises to fetch transaction information from the blockchain. */
-      var promises = [];
-
-      /* Check if nonce has a default value. */
-      if (_options.nonce) { 
-        nonce = _options.nonce; 
-      } else { 
-        promises.push(okdk.web3.eth.getTransactionCount(_from.address)); 
-      }
-
-      /* Check if gasPrice has a default value. */
-      if (_options.gasPrice) { 
-        gasPrice = _options.gasPrice; 
-      } else { 
-        promises.push(okdk.web3.eth.getGasPrice()); 
-      }
-
-      /* Check if gasLimit has a default value. */
-      if (_options.gasLimit) { 
-        gasLimit = _options.gasLimit; 
-      } else { 
-        promises.push(okdk.web3.eth.estimateGas({
-          from: _from.address,
-          to: _to,
-          value: hexValue,
-          data: _data,
-          chainId: okdk.chainId
-        })); 
-      }
-
-      /* If there is a value to resolve, resolve it. */
-      if (promises.length > 0) {
-        const [resolvedNonce, resolvedGasPrice, resolvedGasLimit] 
-          = await Promise.all(promises);
-
-        /* Set resolved values. */
-        if (resolvedNonce) { nonce = resolvedNonce; }
-        if (resolvedGasPrice) { gasPrice = resolvedGasPrice; }
-        if (resolvedGasLimit) { gasLimit = resolvedGasLimit; }
-      }
-
-      const hexValue = okdk.web3.utils.toHex(_value);
-      const hexNonce = okdk.web3.utils.toHex(nonce);
-      const hexGasPrice = okdk.web3.utils.toHex(gasPrice);
-      const hexGasLimit = okdk.web3.utils.toHex(gasLimit);
-      // const hexGasLimit = okdk.web3.utils.toHex(4300000);
-      
-      const transactionResult = await this.sendTransactionWithValues(
-                               _from, _to, hexValue, _data, hexNonce,
-                               hexGasPrice, hexGasLimit, privateKey);
-      return transactionResult;
     } catch (error) {
       throw error;
     }
   };
 
   /**
-   * Create, sign, and send transaction to network.
-   * @ignore
+   * Freeze tokens.
    *
-   * @param {Object} _from - User instance of caller.
-   * @param {string} _to - Address receiving transaction. null if contract deployment.
-   * @param {int} _value - Value of transaction.
-   * @param {Object} _data - Data(function call/contract info) to include in transaction.
-   * @param {int} _nonce - Number of ending transactions by caller in network.
-   * @param {int} _gasPrice - Price per gas used.
-   * @param {int} _gasLimit - Maximum gas to use in transaction.
-   * @param {string} _privateKey - Private key of caller.
+   * @param {Object} _user - User instance of sender.
+   * @param {string} _password - Password to unlock user's keystore.
+   * @param {string} _address - Address to send tokens to.
+   * @param {boolean} _freeze - True to freeze.
+   * @param {Object} _options - Optional object containing values of 
+   *                            nonce, gasPrice, and gasLimit to be used.
    */
-  async sendTransactionWithValues(_from, _to, _value, _data, _nonce,
-                                  _gasPrice, _gasLimit, _privateKey) {
+  async freezeAccount(_user, _password, _address, _freeze, _options = {}) {
     try {
-
-      let rawTx = {
-        from: _from.address,
-        nonce: _nonce,
-        gasPrice: _gasPrice,
-        gasLimit: _gasLimit,
-        to: _to,
-        value: _value,
-        data: _data,
-        chainId: okdk.chainId,
-      };
-
-      // console.log('\n========================',
-      //        '\nCreating and signing the following transaction:',
-      //        '\nfrom:', _from.address, '\nnonce:', _nonce, '\ngasPrice:', parseInt(_gasPrice, 16),
-      //        '\ngasLimit:', parseInt(_gasLimit, 16),
-      //        '\nto:', _to, '\nvalue:', _value, '\nchain id:', okdk.chainId, 
-      //        '\n========================\n');
       
-      const privateKeyBuffer = Buffer.from(_privateKey, 'hex');
+      // Construct abi.
+      let callData =
+        this._contract.methods.freezeAccount(_address, _freeze).encodeABI();
+        
+      const result =
+        await okdk.core.callContract(_user, _password, this._address, callData ,_options);
 
-      const transaction = new ethTx(rawTx);
-      transaction.sign(privateKeyBuffer);
+      return result;
 
-      const serializedTxn = '0x' + transaction.serialize().toString('hex');
-      let transactionId = okdk.web3.utils.sha3(serializedTxn);
-
-      const receipt = await okdk.web3.eth.sendSignedTransaction(serializedTxn);
-
-      return this.constructSuccessJSON(receipt);
     } catch (error) {
       throw error;
     }
   };
 
-  constructSuccessJSON(receipt) {
-    // console.log('Transaction success! Receipt received.');
-    let data = {'success': true,
-                 'receipt': receipt};
-    return data;
+  /**
+   * Convert token units in decimals to actual tokens with float.
+   *
+   * @param {int} _units - Number of tokens represented in smart contract.
+   * @param {int} Actual number of tokens.
+   */
+  unitsToTokens(_units) {
+    const BN_QUINTILLION = okdk.web3.utils.toBN(QUINTILLION);
+    return Math.floor(okdk.web3.utils.toBN(_units).div(BN_QUINTILLION));
   };
 
-  constructFailureJSON(message) {
-    let data = {'success': false,
-                 'message': message};
-    return data;
+  /**
+   * Convert tokens with float to decimal token units.
+   *
+   * @param {float} _tokens - Actual number of tokens.
+   * @param {string} Big number of tokens represented in smart contract.
+   */
+  tokensToUnits(_tokens) {
+    const BN_QUINTILLION = okdk.web3.utils.toBN(QUINTILLION);
+    return okdk.web3.utils.toBN(_tokens).mul(BN_QUINTILLION);
   };
 
 }
+
